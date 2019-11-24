703b45f06f815a1194420074f4dfc13110a7d19c
superbderrick <kang.derrick@gmail.com>
2019-11-24 16:32:06 +0900

    finished a basic click action

--- app/src/main/java/supebderrick/github/screenrecorder/MainActivity.kt
+++ app/src/main/java/supebderrick/github/screenrecorder/MainActivity.kt
@@ -2,11 +2,25 @@ package supebderrick.github.screenrecorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recordingButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recordingButton = findViewById(R.id.recordingButton)
+
+        recordingButton.setOnClickListener {
+            Log.d("derrick" , "Clicked")
+        }
+
     }
 }
+
+
