dcabcd3c0e3cf18718c105b98f8635ce64cd0a0e
superbderrick <kang.derrick@gmail.com>
2019-12-04 16:39:41 +0900

    a little bit modifed

--- app/src/main/java/recoder/MDScreenRecorder.kt
+++ app/src/main/java/recoder/MDScreenRecorder.kt
@@ -2,11 +2,18 @@ package recoder
 
 import android.app.Activity
 import android.content.Context
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
 import android.media.MediaRecorder
+import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.WindowManager
+import androidx.core.app.ActivityCompat.startActivityForResult
 import supebderrick.github.screenrecorder.MainActivity
+import supebderrick.github.screenrecorder.R
+import java.io.IOException
 
 
 class MDScreenRecorder(mediaType: MediaType,
@@ -31,6 +38,9 @@ class MDScreenRecorder(mediaType: MediaType,
     private var mMediaRecorder: MediaRecorder? = null
     private var mContext: Context? = null
     private var mProjectionManager: MediaProjectionManager? = null
+    private var mMediaProjection: MediaProjection? = null
+    private var mVirtualDisplay: VirtualDisplay? = null
+    
 
     init {
         construct()
@@ -55,5 +65,52 @@ class MDScreenRecorder(mediaType: MediaType,
         windowManager.defaultDisplay.getMetrics(metrics)
         MainActivity.mScreenDensity = metrics.densityDpi
         mProjectionManager = mContext?.getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+    }
+
+     private fun prepareMediaRecorder(): Boolean {
+
+        mMediaRecorder = MediaRecorder()
+        mMediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mMediaRecorder?.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+        mMediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        mMediaRecorder?.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+        mMediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        mMediaRecorder?.setVideoEncodingBitRate(512 * 1000)
+        mMediaRecorder?.setVideoFrameRate(30)
+        mMediaRecorder?.setVideoSize(
+                DISPLAY_WIDTH,
+                DISPLAY_HEIGHT
+        )
+
+        
+        //mMediaRecorder?.setOutputFile(mLatestFilepath)
+
+        try {
+            mMediaRecorder?.prepare()
+        } catch (e: IOException) {
+            mMediaRecorder = null
+            return false
+        }
+
+        return true
+    }
+
+    private fun createVirtualDisplay(): VirtualDisplay? {
+        return mMediaProjection?.createVirtualDisplay(
+            "MainActivity",
+            MDScreenRecorder.DISPLAY_WIDTH,
+            MDScreenRecorder.DISPLAY_HEIGHT,
+            MDScreenRecorder.mScreenDensity,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+            mMediaRecorder?.surface, null /*Handler*/, null
+        )
+    }
+
+    private fun requestStartRecordingScreen() {
+
+        (mContext as MainActivity).startActivityForResult(
+            mProjectionManager?.createScreenCaptureIntent(),
+            MDScreenRecorder.PERMISSION_CODE
+        )
     }
 }
--- app/src/main/java/supebderrick/github/screenrecorder/MainActivity.kt
+++ app/src/main/java/supebderrick/github/screenrecorder/MainActivity.kt
@@ -138,7 +138,6 @@ class MainActivity : AppCompatActivity() {
         mVirtualDisplay = createVirtualDisplay()
 
 
-
     }
 
     private fun showDialogPermission() {
